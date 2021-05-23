f994fa9de182670ee53108c4a7b3b97507fdb15f
tradecoder <thetradecoder@gmail.com>
2021-05-23 16:31:23 +0600

    added user login system with bcrypt at server users route

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -29,3 +29,19 @@ router.route(':/register')
     .then(data=>res.send(data))
     .catch(err=>res.send("Registration faild. Please try with a different email and mobile number"))
 })
+
+router.route('/login/:email/:password')
+.post((req, res)=>{
+    const {email, password} = req.params;
+    Users.find({email})
+    .then(user=>{
+        bcrypt.hash(()=>{
+            bcrypt.compare(password, user.password, (err, data)=>{
+                if(data){
+                    res.send({userid:user._id, username:user.email, firstname:user.firstname})
+                }
+            })
+        })
+    })
+    .catch(()=>res.send("Request declined! Try with correct information. "))
+})
