e65f613afb65306b27b9b6522eff5f3106012d4d
tradecoder <thetradecoder@gmail.com>
2021-05-24 16:16:40 +0600

    App update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Signup from './components/signup.component';
+import Login from './components/login.component';
+import Logout from './components/logout.component';
+
 function App() {
   return (
-    <div>
-      <h1>Lottery Winners</h1>
-    </div>
+    <Router>
+      <Route path="/login/" component={Login} />
+      <Route path="/signup/" component={Signup} />
+      <Route path="/logout/" component={Logout}/>
+    </Router>
   );
 }
 
