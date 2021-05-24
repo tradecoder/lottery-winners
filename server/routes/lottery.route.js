d11e7597cc7ec7254be3fcf41d97c231b646776b
tradecoder <thetradecoder@gmail.com>
2021-05-24 14:27:12 +0600

    applied admin data access permission

--- server/routes/lottery.route.js
+++ server/routes/lottery.route.js
@@ -1,5 +1,6 @@
 const router =  require("express").Router();
 const Lottery = require("../models/lottery.model");
+const Users = require("../models/users.model");
 const shortid = require ("shortid");
 
 
@@ -42,6 +43,26 @@ router.route("/user/:userid/")
     Lottery.find({userid}, {_id:0, lotterynumber:1, valid:1,facebookurl:1})
     .then((data)=>{
         res.send(data)
+    })
+    .catch(err=>res.send(err))
+})
+
+router.route("/all/:admin/:userid")
+.get((req, res)=>{
+    const {admin, userid} = req.params;
+    // Although the user is logged in
+    // to prevent any code injection 
+    // applying an extra layer match with admin permission to send all lottery data
+    Users.find({_id:userid, type:admin})
+    .then(user=>{
+        if(user._id){            
+        Lottery.find({}, (data)=>{
+            res.send(data)
+        })      
+
+        } else {
+            res.send("Request declined!")
+        }
     })
     .catch(err=>res.send(err))
 })
