94f14309d020dc83f1a2a7013e8eb716baa1be40
tradecoder <thetradecoder@gmail.com>
2021-05-27 21:22:09 +0600

    created home page

---
+++ src/components/home.component.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+export default function Home(){
+
+    const localUser = localStorage.getItem('localUser');
+    const localUserId = localStorage.getItem('localUserId');
+
+
+    function renderHome(){
+        if(localUser && localUserId){
+            return(
+                <div>
+                    <a href="/new-ticket">Collect a new Ticket</a>
+                </div>
+            )
+
+        }else{
+            return (
+                <div>
+                    <p>Please login to continue</p>
+                </div>
+            )
+        }
+    }
+    return(
+        <div>
+
+        </div>
+    )
+}
